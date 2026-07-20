7c524ed81e9dc6789282f702fbae1e4faa87d72e
Josey·Cao <caoqiaoxi@gmail.com>
2026-07-20 11:41:03 +0800

    Chapter 1: Build FunctionQuest Structure

---
+++ main.py
@@ -0,0 +1,6 @@
+from mymath import square 
+print(square(5))
+print(square(-3))
+print(square(2.5))
+
+
---
+++ mymath/algebra.py
@@ -0,0 +1,20 @@
+"""
+Algebra Module
+作者：Qiaoxi
+Function Quest
+"""
+
+
+def square(x):
+	"""
+	返回 x^2
+	"""
+	return x * x
+
+
+def cube(x):
+	"""
+	返回 x^3
+	"""
+	return x * x * x
+
